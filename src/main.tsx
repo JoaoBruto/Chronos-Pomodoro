ab089c530e5c4af41e42b996f303380ec3668b2a
Joao Rodrigo <joaofreire185710@gmail.com>
2026-01-19 17:05:12 -0300

    Creating my first component in JSX

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import {App} from './App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <div>
+      <App></App>
+      <h1>Olá mundo!</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam velit accusantium fuga facere reiciendis dolor fugit aperiam provident dignissimos itaque magni, ex ratione temporibus molestiae explicabo et ipsa. Perferendis, animi?</p>
+
+    </div>
   </StrictMode>,
 )
