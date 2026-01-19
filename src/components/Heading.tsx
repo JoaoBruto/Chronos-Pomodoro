b8c59180e42a556f19f0f5f813b28a95c9b730de
Joao Rodrigo <joaofreire185710@gmail.com>
2026-01-19 19:07:58 -0300

    Adding CSS Modules

---
+++ src/components/Heading.tsx
@@ -0,0 +1,11 @@
+import styles from './Heading.module.css'
+
+export function Heading() {
+   
+  return(
+    <>
+      <h1 className={styles.Heading}>Olá mundo</h1>
+      <h2 className={styles.purple}>Eai</h2>
+    </>
+  )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import {App} from './App'
 import './Styles/global.css'
 import './Styles/theme.css'
+import { Heading } from './components/Heading'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <div>
-      <App></App>
+    <>
+      <Heading/>
       <h1>Olá mundo!</h1>
       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam velit accusantium fuga facere reiciendis dolor fugit aperiam provident dignissimos itaque magni, ex ratione temporibus molestiae explicabo et ipsa. Perferendis, animi?</p>
 
-    </div>
+    </>
   </StrictMode>,
 )
