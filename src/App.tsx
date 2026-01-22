ee30fc4ee1c179a4981d155fe2f4db5bebc6c5d7
Joao Rodrigo <joaofreire185710@gmail.com>
2026-01-22 17:06:21 -0300

    📚 Creating a structure HTML and CSS for website

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,20 @@ export function App () {
 
   return (
     <>
-      
+      <div className="container-fluid">Menu</div>
+        <div className="container">
+          <div className="content">
+            <section>LOGO</section>
+          </div>
+
+      </div>
+
+      <div className="container">
+        <div className="content">
+          <section>Menu</section>
+        </div>
+
+      </div>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,20 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './Styles/global.css'
 import './Styles/theme.css'
-import { Heading } from './components/Heading.tsx'
+//import { Heading } from './components/Heading.tsx'
 import { TimerIcon } from 'lucide-react'
+import { App } from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <>
-      <Heading>
-        Olá mundo
-        <button>
-            <TimerIcon />
-        </button>
-      </Heading>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam velit accusantium fuga facere reiciendis dolor fugit aperiam provident dignissimos itaque magni, ex ratione temporibus molestiae explicabo et ipsa. Perferendis, animi?</p>
-
+      <App></App>
     </>
   </StrictMode>,
 )
