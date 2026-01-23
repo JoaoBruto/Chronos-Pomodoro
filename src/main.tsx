626a867977b5f2b232f937d88b2f7f4e42a91441
Joao Rodrigo <joaofreire185710@gmail.com>
2026-01-23 17:08:15 -0300

    ✨ Creating components of menu with links and icons

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './Styles/global.css'
-import './Styles/theme.css'
 // import { Heading } from './components/Heading.tsx'
 // import { TimerIcon } from 'lucide-react'
 // import { App } from './App.tsx'
+
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import { Container } from './components/Container/index.tsx'
 import { Logo } from './components/LOGO/index.tsx'
+
+import './Styles/global.css'
+import './Styles/theme.css'
+import { Menu } from './components/LOGO copy/index.tsx'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <>
       <Container>
         <Logo></Logo>
+      </Container>
+
+      <Container>
+        <Menu></Menu>
       </Container>
 
     </>
