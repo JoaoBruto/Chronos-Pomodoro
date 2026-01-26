578d60bcde2f0c9167b70f2f5aefbfe2e0f257c1
Joao Rodrigo <joaofreire185710@gmail.com>
2026-01-26 17:23:12 -0300

    ✨ Creating the Form

--- src/main.tsx
+++ src/main.tsx
@@ -28,6 +28,28 @@ createRoot(document.getElementById('root')!).render(
         <CountDown></CountDown>
       </Container>
 
+      <Container>
+        <form className='form' action=''>
+          <div className='formRow'>
+            <label htmlFor='meuInput'>task</label>
+            <input type="text" id='MeuInput' />
+          </div>
+
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className='formRow'>
+            <p>ciclos</p>
+            <p>0 0 0 0 0</p>
+          </div>
+
+          <div className='formRow'>
+            <button>Enviar</button>
+          </div>
+        </form>
+      </Container>
+
     </>
   </StrictMode>,
 )
