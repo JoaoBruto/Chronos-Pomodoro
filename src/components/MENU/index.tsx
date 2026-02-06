9c9527ff412864d5108d9164de8aa83a49b8f30f
Joao Rodrigo <joaofreire185710@gmail.com>
2026-02-05 22:18:13 -0300

    📚 Capturing link click events with preventDefault for dark and light themes.

--- src/components/MENU/index.tsx
+++ src/components/MENU/index.tsx
@@ -17,6 +17,7 @@ export function Menu() {
   return(
     <>
       <nav className={styles.menu}>
+        <h1>{theme}</h1>
         <a className={styles.menuLink} href='#' aria-label='Ir para a Home' title='Ir para a Home'>
           
           <HouseIcon />
